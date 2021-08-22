03e4792c482ed072352bb51e0970c77dd4ea2fa2
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2021-08-22 02:59:59 +0100

    fix url

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== 'production';
 
 export const server = dev
   ? 'http://localhost:3000'
-  : 'https://gabriel-tutorials-next-js-crash-course.arpadgbondor.vercel.app/';
+  : 'https://gabriel-tutorials-next-js-crash-course.arpadgbondor.vercel.app';
