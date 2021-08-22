de51b229082d8e6dd0f720aa38b614725dd1f38a
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2021-08-22 02:58:17 +0100

    update url

--- config/index.js
+++ config/index.js
@@ -1,3 +1,5 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://????.????';
+export const server = dev
+  ? 'http://localhost:3000'
+  : 'https://gabriel-tutorials-next-js-crash-course.arpadgbondor.vercel.app/';
